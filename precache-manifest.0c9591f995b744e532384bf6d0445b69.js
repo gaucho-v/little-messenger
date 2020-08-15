4f10aeea467a1eccf4e529d2e28c293c8510f461
Vadim Oleynik <53967056+gaucho-v@users.noreply.github.com>
2020-08-15 19:55:31 +0300

    Updates

---
+++ precache-manifest.0c9591f995b744e532384bf6d0445b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d72af31f3d15c12e4d856210159a91a",
+    "url": "/little-messenger/index.html"
+  },
+  {
+    "revision": "b72cb890ad3967e12c76",
+    "url": "/little-messenger/static/css/2.f7963b97.chunk.css"
+  },
+  {
+    "revision": "b72cb890ad3967e12c76",
+    "url": "/little-messenger/static/js/2.082988d9.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/little-messenger/static/js/2.082988d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de18bf064c24b9f53332",
+    "url": "/little-messenger/static/js/main.3d201045.chunk.js"
+  },
+  {
+    "revision": "c9b93728caf58610352c",
+    "url": "/little-messenger/static/js/runtime-main.e57d70b4.js"
+  }
+]);
